9b50084ed8e0ef20b3d02b0cea2f993cf4fffdce
lopesjorge <jorgelopeslopes@outlook.com>
2023-12-27 20:48:13 -0300

    create users

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,29 @@
+const Login = require('../models/LoginModel')
+
 exports.index= (req, res, next) =>{
  res.render('login')
+}
+
+exports.register = async (req, res, next) =>{
+try {
+    const login = new Login(req.body)
+    await login.register()
+
+    if(login.errors.length > 0){
+            req.flash('errors',login.errors)
+            req.session.save(function(){
+            return res.redirect('index')
+        })
+        return
+    }
+    
+    req.flash('sucess','Seu usuário foi criado com sucesso!')
+        req.session.save(function(){
+        return res.redirect('index')
+    })
+} 
+catch (e){
+    console.log(e);
+    return res.render('404')
+    }
 }
