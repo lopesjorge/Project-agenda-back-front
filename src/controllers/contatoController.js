08d88293d11580d8d81b2fcd65fbc7f7027d01d9
lopesjorge <jorgelopeslopes@outlook.com>
2024-02-19 16:28:30 -0300

    criando contatos

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,38 @@
-exports.paginaInicial = (req, res) => {
-  res.send('Obrigado por entrar em contato.');
+const Contato = require('../models/ContatoModel')
+
+exports.index = (req, res) => {
+  res.render('contato', {contato:{}});
 };
+
+exports.register = async (req, res) => {
+  try {   
+  const contato = new Contato(req.body)
+  await contato.register()
+  
+  if(contato.errors.length > 0){
+    req.flash('errors', contato.errors)
+    req.session.save(()=>{
+      res.redirect('back')
+      return
+    })
+  }
+
+  req.flash('success', 'Contato registrado com sucesso!')
+    req.session.save(()=>{
+      res.redirect(`/contato/index/${contato.contato._id}`)
+      return
+    })
+  } 
+  catch(e){
+    console.log(e)
+    res.render('404')
+  }
+ 
+};
+
+exports.editIndex = async function (req, res){
+  if(!req.params.id) return res.send('404')
+  const contato = await Contato.buscaPorId(req.params.id)
+  if(!contato) return res.render('404')
+  res.render('contato',{ contato })
+}
